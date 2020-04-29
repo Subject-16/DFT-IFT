4e98b22ccbe68b5fbb3d978a64880fb6b6441deb
Harmanjot Singh <62550561+Subject-16@users.noreply.github.com>
2020-04-29 12:16:02 +0530

    Add files via upload

---
+++ shifting_in_FT.m
@@ -0,0 +1,17 @@
+close all
+clear all
+% code for understanding for matlab uses fft
+fs = 100;               % sampling frequency
+t = 0:(1/fs):(10-1/fs); % time vector
+S = cos(2*pi*15*t);
+n = length(S);
+X = fft(S);
+f = (0:n-1)*(fs/n);     %frequency range
+power = abs(X).^2/n;    %power
+plot(f,power)
+
+Y = fftshift(X);
+fshift = (-n/2:n/2-1)*(fs/n); % zero-centered frequency range
+powershift = abs(Y).^2/n;     % zero-centered power
+figure;
+plot(fshift,powershift)
