195b09371094dea16679a03f6848b1cb110197ba
Harmanjot Singh <62550561+Subject-16@users.noreply.github.com>
2020-06-18 15:05:02 +0530

    Update Fourier_basics.m

--- Fourier_basics.m
+++ Fourier_basics.m
@@ -3,9 +3,16 @@ clear
 close all
 % script for clearing out any doubt with fourier transforms
 Dx = 0.001;
-x  = 0:Dx:1;
+x  = 0:Dx:1;% Dx specifies the frequencies that can be calculated with this FT
 N  = length(x);
 f  = sin(2*pi*5*x);
 k  = (-N/2:N/2-1)/(N*Dx);
 F  = fftshift(fft(f));
 plot(k,abs(F).^2)
+
+f1 = (ifft(F));
+figure
+plot(x,(f1),'*')
+
+figure
+plot(x,f)
