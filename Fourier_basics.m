0fa6555ac6a59165b2c1cae62121bb07670b2da2
Harmanjot Singh <62550561+Subject-16@users.noreply.github.com>
2020-07-01 12:43:59 +0530

    Update Fourier_basics.m

--- Fourier_basics.m
+++ Fourier_basics.m
@@ -2,18 +2,24 @@ clc
 clear
 close all
 % script for clearing out any doubt with fourier transforms
-Dx = 0.001;
-x  = 0:Dx:1;
+Dx = 0.001;% del
+x  = 0:Dx:1;% Dx specifies the frequencies that can be calculated with this FT
 N  = length(x);
 f  = sin(2*pi*5*x);
-k  = (-N/2:N/2-1)/(N*Dx);
-F  = (fft(f));
-F1 = fftshift(F);
-plot(k,abs(F1).^2)
-
-f1 = (ifft(F));
-figure
-plot(x,(f1),'*')
-
 figure
 plot(x,f)
+title('original')
+
+
+k  = (-N/2:N/2-1)/(N*Dx);
+F1 = fft(f);
+F  = fftshift(F1);
+figure
+plot(k,abs(F).^2)
+title('Fourier spectrum')
+
+f1 = (ifft(F1));
+figure
+plot(x,(f1))
+title('Back tranformed')
+ylim([-1,1])
